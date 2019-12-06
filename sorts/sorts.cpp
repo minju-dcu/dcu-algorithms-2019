d492240d685ab12963960d49b2bca1e618eff96b
minju-dcu <mjmj7258@cu.ac.kr>
2019-12-06 11:45:30 +0900

    1206 버블정렬

--- sorts/sorts.cpp
+++ sorts/sorts.cpp
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#define swap(type,x,y) do{type t=x;x=y;y=t;}while(0)
 
 int main()
 {
 	int a[10] = { 3, 4, 2, 6, 7, 8, 3, 4, 1, 5 };
+	int n = 10;
 
-	for ()
+	for (int i = 0; i < n - 1;i++)
 	{
-		for ()
+		for (int j=n-1;j>i;j--)
 		{
+			if (a[j - 1] > a[j])
+				swap(int, a[j - 1], a[j]);
 		}
 	}
 
-	for ()
+	for (int i = 0; i < n;i++)
 	{
-		printf()
+		printf("a[%d]=%d\n", i, a[i]);
 	}
 }
